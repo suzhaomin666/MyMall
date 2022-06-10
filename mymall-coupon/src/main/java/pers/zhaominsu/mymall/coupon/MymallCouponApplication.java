dc8718554e3d8f76e2fa35e3cdcdc583d84b8ee0
suzhaomin <yfszm666@outlook.com>
2022-06-11 02:27:27 +0800

    successful use coupon service

--- mymall-coupon/src/main/java/pers/zhaominsu/mymall/coupon/MymallCouponApplication.java
+++ mymall-coupon/src/main/java/pers/zhaominsu/mymall/coupon/MymallCouponApplication.java
@@ -1,8 +1,9 @@
 package pers.zhaominsu.mymall.coupon;
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@MapperScan("pers.zhaominsu.mymall.coupon.dao")
 @SpringBootApplication
 public class MymallCouponApplication {
 
